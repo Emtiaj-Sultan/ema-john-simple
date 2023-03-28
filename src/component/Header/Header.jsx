0432c73c078f31fa195d53c1e1ca4a6b17913db7
Emtiaj-Sultan <emtiajs@gmail.com>
2023-03-28 16:38:48 +0600

    header added

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav className="h-20 bg-[#1C2B35] flex justify-between items-center px-28">
+            <img src={logo} alt="" />
+            <div className="text-[17px] leading-[20px] tracking-[0.005em] text-white space-x-8">
+                <a className="hover:text-amber-500" href="/order">Order</a>
+                <a className="hover:text-amber-500" href="/orderPreview">Order Preview</a>
+                <a className="hover:text-amber-500" href="/manageInventory">Manage Inventory</a>
+                <a className="hover:text-amber-500" href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
